971c6997fe7c58e020131106b3a957a12ca8dfe5
jpakey99 <jpakery99@gmail.com>
2020-05-07 00:13:13 -0400

    Created a function for dealing cards.  Next is the rules for passing the cards at the start of the hand

--- hearts.py
+++ hearts.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import random
 
+
 @dataclass
 class Card:
     suit: str
@@ -18,23 +19,23 @@ def create_deck():
 
     for i in range(1, 53):
         if i < 14:
-            cards[i-1] = Card('hearts', i)
+            cards[i - 1] = Card('hearts', i)
         elif 14 <= i < 27:
-            cards[i-1] = Card('Spades', i-13)
+            cards[i - 1] = Card('Spades', i - 13)
         elif 27 <= i < 40:
-            cards[i-1] = Card('Diamonds', i-26)
+            cards[i - 1] = Card('Diamonds', i - 26)
         else:
-            cards[i-1] = Card('Clubs', i-39)
-    deck = Deck(cards, 52)
+            cards[i - 1] = Card('Clubs', i - 39)
+    deck = Deck(cards, 53)
     return deck
 
 
 def shuffle_deck(deck):
     cards = deck.cards
-    shuffled_deck = [Card] * 51
+    shuffled_deck = [Card] * 52
     temp = {set}
     new_card = True
-    for i in range(0, 51):
+    for i in range(0, 52):
         while new_card:
             rand = random.randint(0, 51)
             if rand not in temp:
@@ -45,5 +46,30 @@ def shuffle_deck(deck):
     deck.cards = shuffled_deck
 
 
-deck = create_deck()
-shuffle_deck(deck)
+def deal_cards(deck):
+    player = [Card] * 16
+    player1 = [Card] * 16
+    player2 = [Card] * 16
+    player3 = [Card] * 16
+    cards = deck.cards
+    rotation = 0
+
+    for i in range(0, 52):
+        if i % 4 == 0:
+            player[rotation] = cards[i]
+        elif i % 4 == 1:
+            player1[rotation] = cards[i]
+        elif i % 4 == 2:
+            player2[rotation] = cards[i]
+        else:
+            player3[rotation] = cards[i]
+            rotation += 1
+    return player, player1, player2, player3
+
+
+def hearts():
+    deck = create_deck()
+    shuffle_deck(deck)
+
+
+hearts()
